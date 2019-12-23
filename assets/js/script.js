a6d14cffe6ab87557890df1f85ac6aa8282d2533
Carolina <hidalgo.m.carolina@gmail.com>
2019-12-23 20:07:43 -0300

    added js smooth navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
-$(function () {
+$(function() {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+$(function() {
+  $('a').click(function(event) {
+    console.log('');
+    if (this.hash !== '') {
+      event.preventDefault();
+      var gato = this.hash;
+      $("html, body").animate({
+        scrollTop: $(gato).offset().top
+      }, 800, function() {
+        window.location.hash = gato;
+      });
+    }
+  });
+});
